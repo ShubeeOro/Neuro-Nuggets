eccac5f0f60f1f17b192140518ac4c62e238739c
Kevin Ding <dingkevin2005@gmail.com>
2024-05-14 15:26:20 -0700

    Testing Still

---
+++ api/routes/play.py
@@ -0,0 +1,19 @@
+from flask_login import login_required, current_user
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+
+play = Blueprint('play', __name__)
+
+@play.route('/')
+@login_required
+def start_game():
+    return render_template('pages/game_mode.html')
+
+@play.route('/endless')
+@login_required
+def endless_game():
+    return render_template('pages/endless.html')
+
+@play.route('/solo')
+@login_required
+def solo_game():
+    return render_template('pages/solo.html')
